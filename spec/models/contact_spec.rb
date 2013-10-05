89c9dc15898c775019c0c9c0c1e5e2b58b29b576
rilian <dmitriyis@gmail.com>
2013-10-06 01:50:14 +0300

    basic specs

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Contact do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :phone }
+  it { should validate_presence_of :email }
+
+  it { should belong_to :group }
+end
