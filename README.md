7b78921cce395782db1256dcc001d25459527dba
rilian <dmitriyis@gmail.com>
2013-10-06 13:26:12 +0300

    generic Groups controller specs

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ Application represents an Address Book with grouped contacts
 Functional requirements
 -----------------------
 
-[x] I want to be able to add/edit/delete contacts
-[x] I want to be able to search by name/surname match through my contacts
-[x] I want to be able to group my contacts
-[x] I want to be able to search by group through my contacts
-[x] I want to be able to put any contact in any group (contact can be in only one group)
+* I want to be able to add/edit/delete contacts
+* I want to be able to search by name/surname match through my contacts
+* I want to be able to group my contacts
+* I want to be able to search by group through my contacts
+* I want to be able to put any contact in any group (contact can be in only one group)
 
 Additional requirements (optional)
 ----------------------------------
 
-[_] It should work like one page WEB application and should use AJAX technology, load and submit data without reloading a page.
-[_] It should have client side and server side validation.
-[_] It should have user authentication solution and a user should only have access to their own contacts.
-[_] It should have automated tests for all functionality.
+* It should work like one page WEB application and should use AJAX technology, load and submit data without reloading a page.
+* It should have client side and server side validation.
+* It should have user authentication solution and a user should only have access to their own contacts.
+* It should have automated tests for all functionality.
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,76 @@
 require 'spec_helper'
 
 describe GroupsController do
-  pending
+  describe 'GET index' do
+    before do
+      @group_1 = Group.create(name: 'a')
+      @group_2 = Group.create(name: 'b')
+    end
+
+    it 'renders existing groups' do
+      get :index
+
+      response.status.should == 200
+      expect(assigns(:groups)).to match_array([@group_1, @group_2])
+    end
+  end
+
+  describe 'GET new' do
+    it 'assigns empty Group' do
+      get :new
+
+      response.status.should eq 200
+      expect(assigns(:group).class).to eq Group
+    end
+  end
+
+  describe 'POST create' do
+    it 'creates Group' do
+      post :create, { group: { name: 'a' } }
+
+      response.should redirect_to groups_path
+      expect(Group.count).to eq 1
+      expect(Group.first.name).to eq 'a'
+    end
+  end
+
+  describe 'GET edit' do
+    before do
+      @group = Group.create(name: 'a')
+    end
+
+    it 'assigns existing Group' do
+      get :edit, id: @group.id
+
+      response.status.should eq 200
+      expect(assigns(:group)).to eq @group
+    end
+  end
+
+  describe 'PUT update' do
+    before do
+      @group = Group.create(name: 'a')
+    end
+
+    it 'updates Group' do
+      put :update, { id: @group.id, group: { name: 'b' } }
+
+      response.should redirect_to groups_path
+      expect(Group.count).to eq 1
+      expect(Group.first.name).to eq 'b'
+    end
+  end
+
+  describe 'DELETE destroy' do
+    before do
+      @group = Group.create(name: 'a')
+    end
+
+    it 'deletes Group' do
+      delete :destroy, { id: @group.id }
+
+      response.should redirect_to groups_path
+      expect(Group.count).to eq 0
+    end
+  end
 end
